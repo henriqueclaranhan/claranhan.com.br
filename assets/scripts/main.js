f6e0316a1b2cb66e5d4dc72b28046e5e912c1dca
Henrique Claranhan <henrique.claranhan@ufms.br>
2026-08-17 23:32:36 -0400

    feat: redesign portfolio layout

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,5 @@
-const pageNavEl = document.querySelector("#page-nav");
+const currentYear = new Date().getFullYear();
 
-new IntersectionObserver(
-	([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-	{ threshold: [1] }
-).observe(pageNavEl);
-
-document.querySelector("#experience-years").textContent = new Date().getFullYear() - 2022;
-document.querySelector("#study-years").textContent = new Date().getFullYear() - 2019;
+document.querySelector("#experience-years").textContent = currentYear - 2022;
+document.querySelector("#study-years").textContent = currentYear - 2019;
+document.querySelector("#copyright-year").textContent = currentYear;
